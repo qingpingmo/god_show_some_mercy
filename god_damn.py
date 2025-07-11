c92eb3fbe3b54719de80863c141bc796c2800419
qingpingmo <3321734090@qq.com>
2025-07-11 11:43:52 +0000

    ttt

--- god_damn.py
+++ god_damn.py
@@ -1,1 +1,12 @@
 print("shit!")
+
+def holy_shit():
+    print("holy shit!")
+
+
+def fuck():
+    print("fuck!")
+    holy_shit
+    
+
+    
--- how_dare_you.py
+++ how_dare_you.py
@@ -1,1 +1,27 @@
-print("fuck!")
+from god_damn import holy_shit
+
+def how_dare_you():
+    print("how dare you?")
+    holy_shit
+    
+class shit:
+    def __init__(self):
+        print("shit!")
+        holy_shit
+        
+    def how_dare_you(self):
+        print("how dare you?")
+        holy_shit
+
+class HowDareYou(shit):
+    def __init__(self):
+        print("how dare you?")
+        holy_shit
+        
+    def how_dare_you(self):
+        print("how dare you?")
+        holy_shit
+        
+    def shit_bro(self):
+        
+        pass
